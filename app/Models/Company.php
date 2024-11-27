b56c10fe43574db8bd5f1c98fcdd5aa4a38a5130
JaiveerChavda <jayveersinhchavda555@gmail.com>
2024-11-27 20:02:10 +0530

    defining relationships

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Company extends Model
 {
@@ -11,5 +12,9 @@ class Company extends Model
 
     protected $fillable = ['name','email','logo','address','city','country'];
 
+    public function employees(): HasMany
+    {
+        return $this->hasMany(Employee::class);
+    }
     
 }
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['first_name','last_name','profile','company_id','departmant_id','designation_id'];
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function designation(): BelongsTo
+    {
+        return $this->belongsTo(Designation::class);
+    }
+
+
 }
